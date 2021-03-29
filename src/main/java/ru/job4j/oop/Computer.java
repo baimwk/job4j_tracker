d2723d53045d97e85b343bb6ca0d386be0a7bafb
Darya Leonteva <darya.leonteva@rt.ru>
2021-03-29 22:57:11 +0300

    2. Перегрузка конструктора [#462817]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -5,13 +5,18 @@ public class Computer {
     private int ssd;
     private String cpu;
 
+    public Computer() {}
+
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
     }
-
-    public Computer() {}
 
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
@@ -24,5 +29,7 @@ public class Computer {
         comp.printInfo();
         Computer comp2 = new Computer();
         comp2.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
     }
 }
