867061328c8be9e2de59d918fb02b51cebb11eec
Darya Leonteva <darya.leonteva@rt.ru>
2021-04-09 22:32:26 +0300

    5. Tracker - хранилище [#462834]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,5 +12,15 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = currentDateTime.format(formatter);
         System.out.println("Текущее время: " + currentDateTimeFormat);
+
+        Tracker tracker = new Tracker();
+        System.out.println(tracker.add(item));
+        System.out.println("Find by Id = 1: " + tracker.findById(1));
+
+        Item item1 = new Item();
+        item1.setName("item-1");
+        tracker.add(item1);
+        System.out.println("Find by name item-1: " + tracker.findByName("item-1"));
+        tracker.findAll();
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item findByName(String key) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (key.equals(item.getName())) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                itemsWithoutNull[size] = items[index];
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, size);
     }
 }
