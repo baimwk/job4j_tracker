c388dd76c56abe574bdd2407e7ae4c5c644bcae6
Darya Leonteva <darya.leonteva@rt.ru>
2021-06-10 22:06:16 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #166110] fix

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,12 +8,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() < task.getPriority()) {
-                index += 1;
-            }
-            else {
+            if (element.getPriority() >= task.getPriority()) {
                 break;
             }
+            index += 1;
         }
         this.tasks.add(index, task);
 
