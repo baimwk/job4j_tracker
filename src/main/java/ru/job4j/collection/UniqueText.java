490d5ecec6174a1af3bccdccaf251cc3dd1bfe19
Darya Leonteva <darya.leonteva@rt.ru>
2021-06-14 23:35:42 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.toLowerCase().split(" ");
+        String[] text = duplicateText.toLowerCase().split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String or : origin) {
+            check.add(or);
+        }
+        for (String t : text) {
+            check.contains(t);
+            if (!check.contains(t)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/UniqueTextTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UniqueTextTest {
+    @Test
+    public void isEquals() {
+        String origin = "My cat eats a mouse and milk";
+        String text = "My cat eats milk and a mouse";
+        assertThat(UniqueText.isEquals(origin, text), is(true));
+    }
+
+    @Test
+    public void isNotEquals() {
+        String origin = "My cat eats a mouse";
+        String text = "A mouse is eaten by a cat";
+        assertThat(UniqueText.isEquals(origin, text), is(false));
+    }
+
+    @Test
+    public void isEqualsLower() {
+        String origin = "My cat";
+        String text = "cat my";
+        assertThat(UniqueText.isEquals(origin, text), is(true));
+    }
+}
