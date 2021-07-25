c6ced56d4726a43d30b35d1587acb6e85dd122a0
Darya Leonteva <darya.leonteva@rt.ru>
2021-07-25 12:40:37 +0300

    2.1. Лямбда [#58165 #181326] fix

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -1,19 +1,19 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class FI {
     public static void main(String[] args) {
-        Attachment[] atts = {
-                new Attachment("image 1", 20),
-                new Attachment("image 3", 120),
-                new Attachment("image 2", 23),
-                new Attachment("image 4", 23)
-
-        };
-        Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
-        Comparator<String> cmpSize = (left, right) -> left.length() - right.length();
+        Comparator<Attachment> comparator = (left, right) -> Integer.compare(left.getSize(), right.getSize());
+        Comparator<String> cmpSize = (left, right) -> Integer.compare(left.length(), right.length());
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-        Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
+        Comparator<String> cmpDescSize = (left, right) -> Integer.compare(right.length(), left.length());
+
+        String[] input = {"a", "cfff", "b", "one"};
+        Arrays.sort(input, cmpSize);
+        for (String i : input) {
+            System.out.println(i);
+        }
     }
 }
