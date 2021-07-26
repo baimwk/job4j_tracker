4a87059ebb6ed3a9756f84857a32150927e7d6be
Darya Leonteva <darya.leonteva@rt.ru>
2021-07-26 23:04:33 +0300

    checkstyle fix

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -5,10 +5,13 @@ import java.util.Comparator;
 
 public class FI {
     public static void main(String[] args) {
-        Comparator<Attachment> comparator = (left, right) -> Integer.compare(left.getSize(), right.getSize());
-        Comparator<String> cmpSize = (left, right) -> Integer.compare(left.length(), right.length());
+        Comparator<Attachment> comparator = (left, right)
+                -> Integer.compare(left.getSize(), right.getSize());
+        Comparator<String> cmpSize = (left, right)
+                -> Integer.compare(left.length(), right.length());
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-        Comparator<String> cmpDescSize = (left, right) -> Integer.compare(right.length(), left.length());
+        Comparator<String> cmpDescSize = (left, right)
+                -> Integer.compare(right.length(), left.length());
 
         String[] input = {"a", "cfff", "b", "one"};
         Arrays.sort(input, cmpSize);
