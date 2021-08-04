540262339fa77cae8da3877d1a0bb1beef584e58
Darya Leonteva <darya.leonteva@rt.ru>
2021-08-04 22:15:03 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,6 +7,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted(new AddressAscByCity())
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -24,8 +24,8 @@ public class ProfilesTest {
     @Test
     public void whenCollectThreeAddresses() {
         List<Profile> profiles = List.of(
-                new Profile(new Address("Москва", "Беговая ул.", 12, 44)),
                 new Profile(new Address("Саратов", "Ленина ул.", 55, 20)),
+                new Profile(new Address("Москва", "Беговая ул.", 12, 44)),
                 new Profile(new Address("Челябинск", "Комарова ул.", 3, 1))
         );
         Profiles p = new Profiles();
@@ -34,6 +34,19 @@ public class ProfilesTest {
         expected.add(new Address("Москва", "Беговая ул.", 12, 44));
         expected.add(new Address("Саратов", "Ленина ул.", 55, 20));
         expected.add(new Address("Челябинск", "Комарова ул.", 3, 1));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollectTwoDoubleAddresses() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Москва", "Беговая ул.", 12, 44)),
+                new Profile(new Address("Москва", "Беговая ул.", 12, 44))
+        );
+        Profiles p = new Profiles();
+        List<Address> rsl = p.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Москва", "Беговая ул.", 12, 44));
         assertThat(rsl, is(expected));
     }
 }
